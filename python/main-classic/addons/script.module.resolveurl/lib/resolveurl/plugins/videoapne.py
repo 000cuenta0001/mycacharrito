32d3ed5f813bd1efa49551f496a957454798b937
yosecuales <000cuenta0001@gmail.com>
2019-04-28 13:22:55 +0200

    K

--- python/main-classic/addons/script.module.resolveurl/lib/resolveurl/plugins/videoapne.py
+++ python/main-classic/addons/script.module.resolveurl/lib/resolveurl/plugins/videoapne.py
@@ -1,9 +1,6 @@
-"""
-    OVERALL CREDIT TO:
-        t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
-
-    resolveurl XBMC Addon
-    Copyright (C) 2011 t0mm0
+'''
+    resolveurl Kodi plugin
+    Copyright (C) 2019 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,35 +14,37 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+'''
+
 import re
 from lib import helpers
+from lib import jsunpack
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
-
-class VidloxResolver(ResolveUrl):
-    name = "vidlox"
-    domains = ['vidlox.tv', 'vidlox.me']
-    pattern = '(?://|\.)(vidlox\.(?:tv|me))/(?:embed-)?([0-9a-zA-Z]+)'
-
+class VideoApneResolver(ResolveUrl):
+    name = "videoapne"
+    domains = ["videoapne.co"]
+    pattern = '(?://|\.)(videoapne\.co)/(?:embed-)?([0-9a-zA-Z]+)'
+    
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT}
+        headers = {'User-Agent': common.RAND_UA,
+                   'Referer': web_url}
         html = self.net.http_GET(web_url, headers=headers).content
-
-        if html:
-            _srcs = re.search(r'sources\s*:\s*\[(.+?)\]', html)
-            if _srcs:
-                srcs = helpers.scrape_sources(_srcs.group(1), patterns=['''["'](?P<url>http[^"']+)'''], result_blacklist=['.m3u8'])
-                if srcs:
-                    headers.update({'Referer': web_url})
-                    return helpers.pick_source(srcs) + helpers.append_headers(headers)
-
-        raise ResolverError('Unable to locate link')
+        
+        r = re.search("script'>(eval.*?)</script", html, re.DOTALL)
+        
+        if r:
+            html = jsunpack.unpack(r.group(1))
+            src = re.search('file:\s*"([^"]+m3u8)"',html)
+            if src:
+                return src.group(1) + helpers.append_headers(headers)
+        
+        raise ResolverError('Video cannot be located.')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='http://{host}/{media_id}.html')
