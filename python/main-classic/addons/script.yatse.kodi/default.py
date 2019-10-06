af3d67146861be99f18fde4cbd19e478810c66d7
yosecuales <000cuenta0001@gmail.com>
2019-10-06 09:01:33 +0200

    j

--- python/main-classic/addons/script.yatse.kodi/default.py
+++ python/main-classic/addons/script.yatse.kodi/default.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 import sys
-import xbmcgui
 
-from lib import utils
-from lib import share
+sys.argv.insert(1, 0)  # Stupid hack as calling scripts from JSON does not add script handle
+
+import xbmcgui
+import xbmcaddon
+from lib import share, stream, utils
 from lib.utils import logger, translation
 
 logger.info("Starting script version: %s", utils.ADDON_VERSION)
 
 argument = {}
-for arg in sys.argv[1:]:
+for arg in sys.argv[2:]:
     argInfo = arg.split('=')
     argument[argInfo[0]] = argInfo[1]
 
-sys.argv.insert(1,0) # Stupid hack as calling scripts from JSON does not add script handle
-    
 logger.info("Parameters: %s" % argument)
 
-commands = { 'share' : share.run }
+commands = {
+    'share': share.run,
+    'stream': stream.run
+}
 
-if argument['action'] in commands:
-    commands[argument['action']](argument)
+if 'action' not in argument:
+    xbmcaddon.Addon().openSettings()
 else:
-    logger.error("Command not supported: %s" % argument['action'])
-    xbmcgui.Dialog().ok(utils.ADDON_NAME, translation(32004), translation(32005))
+    if argument['action'] in commands:
+        commands[argument['action']](argument)
+    else:
+        logger.error("Command not supported: %s" % argument['action'])
+        xbmcgui.Dialog().ok(utils.ADDON_NAME, translation(32004), translation(32005))
--- python/main-classic/addons/script.yatse.kodi/lib/share.py
+++ python/main-classic/addons/script.yatse.kodi/lib/share.py
@@ -1,60 +1,121 @@
 # -*- coding: utf-8 -*-
-import utils
-import urlresolver
 import urllib
+
+import private.ydlfix
+import urlresolver
+import utils
 import xbmc
-import xbmcgui
-
+import xbmcaddon
 from utils import logger
+from youtube_dl import YoutubeDL
 
-have_youtube_dl = "false"
-try:
-    from youtube_dl import YoutubeDL
-    have_youtube_dl = "true"
-except ImportError:
-    logger.info("Youtube dl not present")
+private.ydlfix.patch_youtube_dl()
+
+have_adaptive_plugin = '"enabled":true' in xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Addons.GetAddonDetails","id":1,"params":{"addonid":"inputstream.adaptive", "properties": ["enabled"]}}')
+
 
 def run(argument):
     if argument['type'] == 'magnet':
-        openWith = utils.getSetting('openMagnetWith')
-        logger.info('Sharing magnet with %s' % openWith)
-        if openWith == 'Elementum':
-            utils.callPlugin('plugin://plugin.video.elementum/playuri?uri=' + argument['data'] + ')')
-        elif openWith == 'Torrenter V2':
-            utils.callPlugin('plugin://plugin.video.torrenter/?action=playSTRM&url=' + argument['data'] + ')')
-        elif openWith == 'Quasar':
-            utils.callPlugin('plugin://plugin.video.quasar/playuri?uri=' + argument['data'] + ')')
+        handle_magnet(argument['data'])
     elif argument['type'] == 'unresolvedurl':
-        url = urllib.unquote(argument['data'])
-        logger.info('Trying to resolve with urlresolver: %s' % url)
-        stream_url = urlresolver.HostedMediaFile(url = url).resolve()
-        if not stream_url:
-            if have_youtube_dl == "false":
-                logger.error("Url not resolved and no YoutubeDL: %s" % url)
-            else:
-                logger.info('Trying to resolve with YoutubeDL: %s' % url)
-                YoutubeDL_resolver = YoutubeDL({'format': 'best'})
-                YoutubeDL_resolver.add_default_info_extractors()
-                result = YoutubeDL_resolver.extract_info(url, download = False)
-                if "url" in result:
-                    stream_url = result["url"]
-                    logger.info("Url resolved by YoutubeDL: %s" % result)
-                else:
-                    logger.error("Url not resolved: %s" % url)
+        if ('queue' not in argument) or (argument['queue'] == 'false'):
+            action = 'play'
         else:
-            logger.info("Url resolved by urlResolver: %s" % stream_url)
+            action = 'queue'
+        handle_unresolved_url(argument['data'], action)
 
-        if stream_url:
-            if (not 'queue' in argument) or (argument['queue'] == "false") or (not xbmc.Player().isPlaying()):
-                logger.info('Playing resolved url: %s' % stream_url)
-                xbmc.Player().play(stream_url)
-            else:
-                if xbmc.Player().isPlayingAudio():
-                    logger.info('Queuing to music resolved url: %s' % stream_url)
-                    xbmc.PlayList(xbmc.PLAYLIST_MUSIC).add(stream_url)
-                else:
-                    logger.info('Queuing to video resolved url: %s' % stream_url)
-                    xbmc.PlayList(xbmc.PLAYLIST_VIDEO).add(stream_url)
+
+def handle_magnet(data):
+    open_with = utils.get_setting('openMagnetWith')
+    logger.info('Sharing magnet with %s' % open_with)
+    if open_with == 'Elementum':
+        utils.call_plugin('plugin://plugin.video.elementum/playuri?uri=' + data)
+    elif open_with == 'Torrenter V2':
+        utils.call_plugin('plugin://plugin.video.torrenter/?action=playSTRM&url=' + data)
+    elif open_with == 'Quasar':
+        utils.call_plugin('plugin://plugin.video.quasar/playuri?uri=' + data)
+    elif open_with == 'YATP':
+        utils.call_plugin('plugin://plugin.video.yatp/?action=play&torrent=' + data + '&file_index=dialog')
+
+
+def resolve_with_youtube_dl(url, parameters, action):
+    youtube_dl_resolver = YoutubeDL(parameters)
+    youtube_dl_resolver.add_default_info_extractors()
+    try:
+        result = youtube_dl_resolver.extract_info(url, download=False)
+        if result is None:
+            result = {}
+    except Exception as e:
+        logger.error(u'Error with YoutubeDL: %s' % e)
+        result = {}
+    logger.info(u'YoutubeDL full result: %s' % result)
+    if 'entries' in result:
+        logger.info(u'Playlist resolved by YoutubeDL: %s items' % len(result['entries']))
+        item_list = []
+        for entry in result['entries']:
+            if entry is not None and 'url' in entry:
+                item_list.append(entry)
+                logger.info(u'Media found: %s' % entry['url'])
+        if len(item_list) > 0:
+            utils.play_items(item_list, action)
+            return True
         else:
-            dialog = xbmcgui.Dialog()
-            dialog.notification(utils.ADDON_NAME, utils.translation(32006), xbmcgui.NOTIFICATION_INFO, 5000)
+            logger.info(u'No playable urls in the playlist')
+    if 'url' in result:
+        logger.info(u'Url resolved by YoutubeDL: %s' % result['url'])
+        utils.play_url(result['url'], action, result)
+        return True
+    if 'requested_formats' in result:
+        if have_adaptive_plugin:
+            logger.info(u'Adaptive plugin enabled looking for dash content')
+            for entry in result['requested_formats']:
+                if 'container' in entry and 'manifest_url' in entry:
+                    if 'dash' in entry['container']:
+                        logger.info(u'Url resolved by YoutubeDL: %s' % entry['manifest_url'])
+                        utils.play_url(entry['manifest_url'], action, result, True)
+                        return True
+        for entry in result['requested_formats']:
+            if 'protocol' in entry and 'manifest_url' in entry:
+                if 'm3u8' in entry['protocol']:
+                    logger.info(u'Url resolved by YoutubeDL: %s' % entry['manifest_url'])
+                    utils.play_url(entry['manifest_url'], action, result)
+                    return True
+    return False
+
+
+def handle_unresolved_url(data, action):
+    url = urllib.unquote(data)
+    logger.info(u'Trying to resolve URL (%s): %s' % (action, url))
+    if xbmc.Player().isPlaying():
+        utils.show_info_notification(utils.translation(32007), 1000)
+    else:
+        utils.show_info_notification(utils.translation(32007))
+    if 'youtube.com' in url or 'youtu.be' in url:
+        youtube_addon = xbmcaddon.Addon(id="plugin.video.youtube")
+        if youtube_addon:
+            if youtube_addon.getSetting("kodion.video.quality.mpd") == "true":
+                logger.info(u'Youtube addon have DASH enabled use it')
+                utils.play_url('plugin://plugin.video.youtube/uri2addon/?uri=%s' % url, action)
+                return
+    logger.info(u'Trying to resolve with YoutubeDL')
+    result = resolve_with_youtube_dl(url, {'format': 'best', 'no_color': 'true', 'ignoreerrors': 'true'}, action)
+    if result:
+        return
+    # Second pass with new params to fix site like reddit dash streams
+    logger.info(u'Trying to resolve with YoutubeDL other options')
+    result = resolve_with_youtube_dl(url, {'format': 'bestvideo+bestaudio/best', 'no_color': 'true', 'ignoreerrors': 'true'}, action)
+    if result:
+        return
+    logger.error(u'Url not resolved by YoutubeDL')
+
+    logger.info(u'Trying to resolve with urlResolver')
+    stream_url = urlresolver.HostedMediaFile(url=url).resolve()
+    if stream_url:
+        logger.info(u'Url resolved by urlResolver: %s' % stream_url)
+        utils.play_url(stream_url, action)
+        return
+
+    logger.info(u'Trying to play as basic url')
+    utils.play_url(url, action)
+    if url:
+        utils.show_error_notification(utils.translation(32006))
