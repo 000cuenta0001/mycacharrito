92b4b8c6765eb547d9904be981d40b05b1b13551
yosecuales <000cuenta0001@gmail.com>
2026-09-16 06:22:28 +0200

    Update main.py

--- python/main-classic/addons/plugin.video.totalsports/resources/lib/main.py
+++ python/main-classic/addons/plugin.video.totalsports/resources/lib/main.py
@@ -7,59 +7,31 @@ import xbmcgui
 import xbmcplugin
 import xbmcaddon
 
-# -----------------------------
 # Compatibilidad Py2 / Py3
-# -----------------------------
 try:
     import urllib.request as urllib_request
-    from urllib.parse import quote, unquote
 except ImportError:
     import urllib2 as urllib_request
-    from urllib import quote, unquote
 
 addon = xbmcaddon.Addon()
 HANDLE = int(sys.argv[1])
 
-LIST_URL = "https://dl.dropboxusercontent.com/scl/fi/0khvwyoah0xbosfcc85p7/lista1AAA_IPTV.m3u?rlkey=ytvn2dprcdzrpgl5797elm43l&st=o2jaa1ge&dl=0"
+LIST_URL = "https://dl.dropboxusercontent.com/scl/fi/ua0k1zunodoeulerkysf4/lista1AAA_IPTV.m3u?rlkey=4h35nwc7nm78m93yh5bke0pjg&st=7wrpyc23&dl=0"
 
 ADDON_ICON = addon.getAddonInfo("icon")
 ADDON_FANART = addon.getAddonInfo("fanart")
 
 
 # ------------------------------------------------------------
-# UTILIDADES UNICODE SEGURAS (Py2 / Py3)
-# ------------------------------------------------------------
-def safe_quote(value):
-    try:
-        # Python 2
-        if isinstance(value, unicode):
-            value = value.encode("utf-8")
-    except NameError:
-        # Python 3
-        if isinstance(value, str):
-            value = value.encode("utf-8")
-    return quote(value, safe="")
-
-
-def safe_unquote(value):
-    value = unquote(value)
-    try:
-        return value.decode("utf-8")
-    except:
-        return value
-
-
-# ------------------------------------------------------------
-# DESCARGA FORZADA (SIN CACHE)
+# DESCARGA FORZADA (sin cache)
 # ------------------------------------------------------------
 def get_remote_data(url):
     try:
-        sep = "&" if "?" in url else "?"
+        sep = '&' if '?' in url else '?'
         url = "%s%st=%d" % (url, sep, int(time.time()))
         response = urllib_request.urlopen(url, timeout=10)
         data = response.read()
         response.close()
-
         if isinstance(data, bytes):
             data = data.decode("utf-8", "ignore")
         return data
@@ -68,7 +40,7 @@ def get_remote_data(url):
 
 
 # ------------------------------------------------------------
-# PARSER M3U (MÍNIMO NECESARIO)
+# PARSER M3U (solo lo necesario)
 # ------------------------------------------------------------
 def parse_m3u(data):
     items = []
@@ -110,18 +82,9 @@ def list_videos():
     items = parse_m3u(data)
 
     for ch in items:
-        title = ch["title"]
-        url = ch["url"]
+        li = xbmcgui.ListItem(label=ch["title"])
 
-        li = xbmcgui.ListItem(label=title)
-
-        # NO forzar info técnica (codec, resolución, etc.)
-        try:
-            tag = li.getVideoInfoTag()
-            tag.setTitle(title)
-            tag.setMediaType("video")
-        except:
-            pass
+        li.setProperty("IsPlayable", "true")
 
         li.setArt({
             "icon": ch["logo"] or ADDON_ICON,
@@ -131,10 +94,9 @@ def list_videos():
 
         play_url = (
             sys.argv[0]
-            + "?play="
-            + safe_quote(url)
-            + "&title="
-            + safe_quote(title)
+            + "?play=1"
+            + "&url=" + urllib_request.quote(ch["url"], safe="")
+            + "&title=" + urllib_request.quote(ch["title"], safe="")
         )
 
         xbmcplugin.addDirectoryItem(
@@ -153,44 +115,34 @@ def list_videos():
 
 
 # ------------------------------------------------------------
-# REPRODUCCIÓN (NOMBRE CORRECTO DEL CANAL)
+# REPRODUCCIÓN (PATRÓN CORRECTO)
 # ------------------------------------------------------------
 def play(params):
-    url = params.get("play")
-    title = params.get("title", "")
+    url = params.get("url", "")
+    title = params.get("title", "Canal")
 
-    li = xbmcgui.ListItem(label=title)
+    li = xbmcgui.ListItem(label=title, path=url)
+    li.setProperty("IsPlayable", "true")
 
-    try:
-        tag = li.getVideoInfoTag()
-        tag.setTitle(title)
-        tag.setMediaType("video")
-    except:
-        pass
-
-    xbmc.Player().play(url, li)
+    xbmcplugin.setResolvedUrl(HANDLE, True, li)
 
 
 # ------------------------------------------------------------
 # ROUTER
 # ------------------------------------------------------------
-def router(paramstring):
+def get_params():
     params = {}
-
-    if paramstring:
-        for p in paramstring[1:].split("&"):
+    if len(sys.argv) > 2 and sys.argv[2]:
+        for p in sys.argv[2][1:].split("&"):
             if "=" in p:
                 k, v = p.split("=", 1)
-                params[k] = safe_unquote(v)
+                params[k] = urllib_request.unquote(v)
+    return params
 
-    if "play" in params:
+
+if __name__ == "__main__":
+    params = get_params()
+    if params.get("play"):
         play(params)
     else:
         list_videos()
-
-
-# ------------------------------------------------------------
-# MAIN
-# ------------------------------------------------------------
-if __name__ == "__main__":
-    router(sys.argv[2])
